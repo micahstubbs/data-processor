0da3b7dd84c5a4c9bd92f58afd868e12d0c10f99
Christopher Shea <cmshea@gmail.com>
2016-10-31 13:58:18 -0400

    Update await statistics now for bounded-batch-process

--- src/vip/data_processor/errors.clj
+++ src/vip/data_processor/errors.clj
@@ -1,5 +1,6 @@
 (ns vip.data-processor.errors
-  (:require [clojure.core.async :as a]))
+  (:require [clojure.core.async :as a]
+            [clojure.tools.logging :as log]))
 
 (defrecord ValidationError [ctx severity scope
                             identifier error-type error-value])
@@ -17,6 +18,19 @@
   (a/close! errors-chan)
   ctx)
 
-(defn await-statistics [ctx]
-  (a/<!! (:processing-chan ctx))
+(defn await-statistics
+  "the process validations functions use bounded-batch-process from
+  utility-works.async and puts the resulting core.async channel on the
+  context at :processing-chan. Once the last validations have been
+  saved, a thread will be created to calculate the feed's
+  statistics. That thread's value will be a future that is placed on
+  the core.async channel. Thus, here we wait on the core.async
+  channel, and then wait on the future it gives."
+  [ctx]
+  (log/info "Awaiting statistics")
+  (-> ctx
+      :processing-chan
+      a/<!!
+      deref)
+  (log/info "Statistics complete")
   ctx)
